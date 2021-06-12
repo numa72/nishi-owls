59c920a365f98a8d7ff9124292cb120f0d9aa7b9
Hiroki Sato <hsugar.310@gmail.com>
2021-06-12 12:42:12 +0900

    basic designs

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+	// nav bar
+	$("ul.sub").hide();
+	$("#menu li").hover(function(){
+		$("ul:not(:animated)",this).slideDown("fast");
+	},
+	function(){
+		$("ul",this).slideUp("fast");
+	});
+});
