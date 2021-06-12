0142a81af61ac1ac78626f8defc859c3780980c9
Hiroki Sato <hsugar.310@gmail.com>
2021-06-12 12:52:51 +0900

    Format files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
-$(function(){
+$(function () {
 	// nav bar
 	$("ul.sub").hide();
-	$("#menu li").hover(function(){
-		$("ul:not(:animated)",this).slideDown("fast");
-	},
-	function(){
-		$("ul",this).slideUp("fast");
-	});
+	$("#menu li").hover(
+		function () {
+			$("ul:not(:animated)", this).slideDown("fast");
+		},
+		function () {
+			$("ul", this).slideUp("fast");
+		});
 });
