6b379a8a6f073a63a35eb16d2f45010b59a9e7a1
Hiroki Sato <hsugar.310@gmail.com>
2021-07-22 18:57:59 +0900

    game report with accordion-menued

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,4 +8,11 @@ $(function () {
 		function () {
 			$("ul", this).slideUp("fast");
 		});
+
+	// game report accordion.
+	$(".game-report-detail h3").next().slideToggle(0);
+	$(".game-report-detail h3").on('click', function () {
+		$(this).next().slideToggle(500);
+		$(this).toggleClass('open', 500);
+	})
 });
