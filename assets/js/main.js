8d7ba9feeac12c5ffa10c39a9015609ee63e3deb
Hiroki Sato <hsugar.310@gmail.com>
2021-08-08 00:32:18 +0900

    adjust styles in toppage

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,27 @@ $(function () {
 		$(this).toggleClass('open', 500);
 	})
 });
+
+$(function () {
+	// top page games horizontal dragging.
+	const gamesSlider = $('.top-games');
+	let isMouseDown = false;
+	let startX;
+	let scrollX;
+	gamesSlider.on('mousedown', (e) => {
+		isMouseDown = true;
+		startX = e.pageX;
+		scrollX = gamesSlider.scrollLeft();
+	});
+	gamesSlider.on('mouseleave', () => {
+		isMouseDown = false;
+	});
+	gamesSlider.on('mouseup', () => {
+		isMouseDown = false;
+	})
+	gamesSlider.on('mousemove', (e) => {
+		if (!isMouseDown) return;
+		e.preventDefault();
+		gamesSlider.scrollLeft(scrollX + (e.pageX - startX));
+	})
+});
